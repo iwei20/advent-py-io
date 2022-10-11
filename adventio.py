bb32fe77d0ad3a10737c4e96457273688a1fb571
iwei20 <44007607+iwei20@users.noreply.github.com>
2022-10-11 18:22:58 -0400

    Implement some of stdinstream

--- adventio.py
+++ adventio.py
@@ -1,8 +1,10 @@
 import os
 import sys
 from atexit import register
+from collections import deque
 from io import BytesIO
-from typing import List
+from typing import Generator
+from typing_extensions import Self
 
 sys.stdin = BytesIO(os.read(0, os.fstat(0).st_size))
 sys.stdout = BytesIO()
@@ -10,9 +12,49 @@ register(lambda: os.write(1, sys.stdout.getvalue()))
 
 input = lambda: sys.stdin.readline().rstrip('\r\n')
 
-def inlist(n: int) -> List[int]:
-    """
-    Reads the next n integers from input and returns them in a list
-    """
-    return [5]
+class STDINStream:
+    def __init__(self: Self):
+        self.input_deque = deque()
+
+    def next(self: Self) -> str:
+        """
+        Returns the next string token from stdin
+        """
+        if self.input_deque.empty():
+            self.input_deque.extend(input().split())
+        return self.input_deque.popleft()     
+
+    def next_int(self: Self) -> str:
+        """
+        Returns the next integer from stdin
+        """
+        return int(self.next())
+
+    def next_float(self: Self) -> float:
+        """
+        Returns the next float from stdin
+        """
+        return float(self.next())
+
+    def next_line(self: Self) -> str:
+        """
+        Discards progress on the current line, then consumes and returns the next line
+        """
+        self.input_deque.clear()
+        return input()
+
+    def next_n(self: Self, n: int) -> Generator[str, None, None]:
+        """
+        Reads the next n tokens from input and returns them in a generator
+        """
+        for _ in range(n):
+            yield self.next()
+
+    def next_n_int(self: Self, n: int) -> Generator[int, None, None]:
+        """
+        Reads the next n integers from input and returns them in a list
+        """
+        for _ in range(n):
+            yield self.next_int()
+    
 
